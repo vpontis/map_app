ddb00147cd569b83d02c050e7cb4a76c5f97ac8e
Akash Badshah <akashbad4123@gmail.com>
2012-09-25 14:13:28 -0400

    created the rails app

---
+++ map_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapApp::Application.config.secret_token = '1ded5f4f8cae7d745e15144c5be28f6c8e89cc9c5fbed1d97e4d25e69cef599ba8a0fd3681d469d5012be62d4ed60838900794738aab64618ef1983568ec1ebe'
